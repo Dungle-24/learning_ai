039dd6813041977266a571bc7a1262ef647a17a3
Hoàng Dũng <hungtamdunghieu@gmail.com>
2025-07-17 22:51:54 +0700

    Update btap_list_tim_so.py

--- lecture-list/btap_list_tim_so.py
+++ lecture-list/btap_list_tim_so.py
@@ -13,9 +13,18 @@ print(max(numbers))
 
 #cau2
 numbers = [20, 10, -4, 5, 15, 36, -16]
-n = list(set(numbers))  
-n.sort(reverse=True)
-print("Số lớn thứ nhì:", n[1])
+
+max1 = max(numbers)
+n = numbers.copy()
+while max1 in n:
+    n.remove(max1)
+#tìm số lớn nhì
+if n:
+    max2 = max(n)
+    print("Số lớn nhì là:", max2)
+else:
+    print("list toàn giống nhau")
+
 
 #cau 3
 # numbers = [20, 36, 10, -4, 5, 15, 36, -16, 36]
