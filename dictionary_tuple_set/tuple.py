f0734dcfa6871a504411c146e2c8a2a460cdba24
Hoàng Dũng <hungtamdunghieu@gmail.com>
2025-07-18 23:06:00 +0700

    Update tuple.py

--- dictionary_tuple_set/tuple.py
+++ dictionary_tuple_set/tuple.py
@@ -24,3 +24,49 @@ print(data)
 data = ("red", "green", "blue", "red")
 print(data.count("red"))   
 print(data.count("black")) 
+
+
+"""
+Cho một danh sách gồm nhiều tuple, mỗi tuple chứa tên học sinh và điểm của học sinh đó.
+
+Hãy viết chương trình để:
+
+1. Tính điểm trung bình của mỗi học sinh.
+
+2. Lưu kết quả vào một dictionary với tên học sinh là key, và điểm trung bình là value.
+
+3. In ra danh sách các học sinh có điểm trung bình lớn hơn hoặc bằng 5.
+input:
+students_scores = [
+    ("An", [4, 6, 5]),
+    ("Bình", [9, 8, 10]),
+    ("Cường", [2, 3, 4]),
+    ("Dung", [7, 5, 6])
+]
+output:
+{'An': 5.0, 'Bình': 9.0, 'Cường': 3.0, 'Dung': 6.0}
+Học sinh có điểm >= 5:
+An: 5.0
+Bình: 9.0
+Dung: 6.0
+"""
+students_scores = [
+    ("An", [4, 6, 5]),
+    ("Bình", [9, 8, 10]),
+    ("Cường", [2, 3, 4]),
+    ("Dung", [7, 5, 6])
+]
+
+diem_tb = {}
+
+for ten, diem_list in students_scores:
+    trung_binh = sum(diem_list) / len(diem_list)
+    diem_tb[ten] = round(trung_binh, 1)
+
+print(diem_tb)
+
+print("\nHọc sinh có điểm >= 5:")
+for ten, tb in diem_tb.items():
+    if tb >= 5:
+        print(f"{ten}: {tb}")
+
