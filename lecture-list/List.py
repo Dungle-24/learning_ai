cf9bcc30e23bb5361b04d48df41b8041be323fed
Hoàng Dũng <hungtamdunghieu@gmail.com>
2025-07-20 23:18:09 +0700

    Update List.py

--- lecture-list/List.py
+++ lecture-list/List.py
@@ -56,3 +56,25 @@ dataset = [2, 4, 5, 7, 9, 223, 11, 35, 23, 11, 93]
 print(dataset.index(11))
 print(sorted(dataset)) #sắp xếp thứ tự tăng dần
 print(list(reversed(sorted(dataset))))
+
+"""
+    Viết 1 hàm nhận 2 tham số đầu vào:
+    1. 1 list bao gồm các số bất kì
+    2. 1 số tự nhiên k
+    Đếm xem số k xuất hiện trong list bao nhiêu lần
+"""
+
+numbers = [20, 10, -4, 5, 10, 36, -16, 3, 5, 10, 16, -5, 5]
+k = 10
+
+def check (number, k):
+    tong = 0
+    for j in number:
+        if j == k:
+            tong +=1
+    print(tong)
+check(numbers,10)
+#cách 2
+def check(numbers, k):
+    print(numbers.count(k))
+check(numbers,10)
