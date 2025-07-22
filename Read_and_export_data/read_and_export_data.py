f6329e8b8dc51b648b5177bc545f93264386c952
Hoàng Dũng <hungtamdunghieu@gmail.com>
2025-07-22 22:22:38 +0700

    Update read_and_export_data.py

--- Read_and_export_data/read_and_export_data.py
+++ Read_and_export_data/read_and_export_data.py
@@ -3,14 +3,24 @@
 #đường dẫn tương đối: đường dẫn từ chỗ mình đang ở tới chỗ mình cần
 relative_path = "./src/dataset.py"
 # đường dẫn tuyệt đối: từ gốc đến nơi mình muốn
-absolute_path = "C:\\Users\\home\\Downloads\\Group4.docx"
+absolute_path = "C:\\Users\\home\\Downloads\\Assignment_4 (3)\\hoàng dung.txt"
 #f = open(file_path, mode)
-#"r": đọc (mặc định)
-#"w": Ghi (ghi đè nọi dung cũ néu có)
-#"a": Ghi thêm (append)
-#"r+": Đọc và ghi (ghi đè nọi dung cũ néu có)
-# "x": Tạo file mới, lỗi nếu file đã tồn tại
-f = open(absolute_path,"r")
 
-print(f.read()) #đọc hết
-print(f.readline()) #đọc theo dòng
+f = open(absolute_path,"r", encoding="utf-8")
+a = open(absolute_path,"a")
+a.write('siuuuu')
+# print(f.read()) #đọc hết
+# print(f.readline()) #đọc theo dòng
+# print(f.readline())
+# print(f.readline())
+print(list(f))
+
+w = open(absolute_path,"w", encoding="utf-8")
+w.writelines(["today is a beautiful day\n", "aa\n", "bb\n"])
+
+f.close() #đóng file
+w.close()
+a.close()
+
+
+
