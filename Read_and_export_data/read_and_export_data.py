add19930d2a8f2ead008c503686819b89d2d6c78
Hoàng Dũng <hungtamdunghieu@gmail.com>
2025-07-22 22:27:40 +0700

    Update read_and_export_data.py

--- Read_and_export_data/read_and_export_data.py
+++ Read_and_export_data/read_and_export_data.py
@@ -9,6 +9,7 @@ absolute_path = "C:\\Users\\home\\Downloads\\Assignment_4 (3)\\hoàng dung.txt"
 f = open(absolute_path,"r", encoding="utf-8")
 a = open(absolute_path,"a")
 a.write('siuuuu')
+
 # print(f.read()) #đọc hết
 # print(f.readline()) #đọc theo dòng
 # print(f.readline())
@@ -18,9 +19,18 @@ print(list(f))
 w = open(absolute_path,"w", encoding="utf-8")
 w.writelines(["today is a beautiful day\n", "aa\n", "bb\n"])
 
+
 f.close() #đóng file
 w.close()
 a.close()
 
+absolute_path = "C:\\Users\\home\\Downloads\\Assignment_4 (3)\\hoàng dung.txt"
+#đóng file = with
+with open(absolute_path, 'r') as f:
+    print(f.readline())
+    print(f.readline())
+    print(f.readline())
+    print(f.readline())
+    print(f.readline())
 
 
